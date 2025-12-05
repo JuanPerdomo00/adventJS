5744c78deeabf35cc70ef45a6245da02422510df
Jakepys <perdomojuan187@gmail.com>
2025-12-05 11:04:57 -0500

    Delete 2025/day_4/day_4/target directory

--- 2025/day_4/day_4/target/debug/day_4.d
+++
@@ -1,1 +0,0 @@
-/home/jakepys/repos/adventJS/2025/day_4/day_4/target/debug/day_4: /home/jakepys/repos/adventJS/2025/day_4/day_4/src/main.rs
--- 2025/day_4/day_4/target/debug/deps/day_4-ccd92cfac4d739b3.d
+++
@@ -1,5 +0,0 @@
-/home/jakepys/repos/adventJS/2025/day_4/day_4/target/debug/deps/day_4-ccd92cfac4d739b3.d: src/main.rs
-
-/home/jakepys/repos/adventJS/2025/day_4/day_4/target/debug/deps/libday_4-ccd92cfac4d739b3.rmeta: src/main.rs
-
-src/main.rs:
